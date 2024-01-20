756cabc1e5c023e9a5c457b712c0ffc745242d43
jan iversen <jancasacondor@gmail.com>
2024-01-20 10:48:24 +0100

    Casa 2024.

--- jellyfin/data/ScheduledTasks/f9b057c0-54e9-e6da-ee4a-88ffd146a403.js
+++ jellyfin/data/ScheduledTasks/f9b057c0-54e9-e6da-ee4a-88ffd146a403.js
@@ -1,1 +1,1 @@
-{"StartTimeUtc":"2024-01-19T16:19:15.713763Z","EndTimeUtc":"2024-01-19T16:19:18.269363Z","Status":"Completed","Name":"Actualizar extensiones","Key":"PluginUpdates","Id":"f9b057c054e9e6daee4a88ffd146a403"}
+{"StartTimeUtc":"2024-01-20T09:39:07.0359509Z","EndTimeUtc":"2024-01-20T09:39:09.554263Z","Status":"Completed","Name":"Actualizar extensiones","Key":"PluginUpdates","Id":"f9b057c054e9e6daee4a88ffd146a403"}
