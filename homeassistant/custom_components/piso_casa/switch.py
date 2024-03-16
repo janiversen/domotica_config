e1699b59ecfb55b585375d29caed3ecb87a8959e
jan iversen <jancasacondor@gmail.com>
2024-03-16 18:29:14 +0100

    switch LED.

--- homeassistant/custom_components/piso_casa/switch.py
+++ homeassistant/custom_components/piso_casa/switch.py
@@ -1,5 +1,6 @@
 """Switch for piso and casa"""
 from homeassistant.components.switch import SwitchDeviceClass
+from homeassistant.helpers.event import async_track_state_change_event
 
 from .base import _LOGGER, CONF_CASA, CalcSwitch
 
@@ -13,6 +14,22 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
     if casa:
         switches.extend([])
     else:
-        switches.extend([])
+        switches.extend([piso_led(hass, "salon"), piso_led(hass, "dormitorio")])
     if switches:
         async_add_entities(switches, True)
+
+
+class piso_led(CalcSwitch):
+    """Control LED dormitorio/salon."""
+
+    def __init__(self, hass, where):
+        """Setup class."""
+        self.my_name = where + "_rgb"
+        self.rest_name = self.my_name + "_rest"
+        super().__init__(hass, self.rest_name, SwitchDeviceClass.SWITCH)
+        # async_track_state_change_event(
+        #     self.hass, ["sensor.production"], self.async_track_production
+        # )
+        # async_track_state_change_event(
+        #     self.hass, ["sensor.power_meter"], self.async_track_power_meter
+        # )
